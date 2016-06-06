b789961f8a4e014d99a0c6234e02fa7b6d459ae9
root <jonasvanreeth@gmail.com>
2016-06-06 11:05:33 +0200

    cv

--- resources/views/home/modals/modal-projects.blade.php
+++ resources/views/home/modals/modal-projects.blade.php
@@ -10,29 +10,29 @@
             </div>
             <div class="modal-body">
                 <div class="row">
-                    <div class="col-md-5 text-center padding-10 margin-20 shadow-black">
+                    <div class="col-md-5 text-center padding-10 margin-20 shadow-black max-height-300">
                         <h4>Locked or Not</h4>
                         <p> Iot project om na te kijken of uw auto op slot is of niet</p>
                     </div>
-                    <div class="col-md-5 text-center padding-10 margin-20 shadow-black">
+                    <div class="col-md-5 text-center padding-10 margin-20 shadow-black max-height-300">
                         <h4>Kot van de Toekomst</h4>
                         <p> Iot project om het kotleven aangenamer te maken
                             en concepten als het huis van de toekomst toegankelijk te maken</p>
                     </div>
-                    <div class="col-md-5 text-center padding-10 margin-20 shadow-black">
+                    <div class="col-md-5 text-center padding-10 margin-20 shadow-black max-height-300">
                         <h4>sjotterkas project</h4>
                         <p> Het automatiseren van digitaliseren van een voetbaltafel.
                             Scoredetectie, Top scores, Rankings,...</p>
 
                     </div>
-                    <div class="col-md-5 text-center padding-10 margin-20 shadow-black">
+                    <div class="col-md-5 text-center padding-10 margin-20 shadow-black max-height-300">
                         <h4>politie - timesheet</h4>
                         <p> project om de timesheets van de politie van excel naar web te krijgen</p>
                     </div>
-                    <div class="col-md-5 text-center padding-10 margin-20 shadow-black">
+                    <div class="col-md-5 text-center padding-10 margin-20 shadow-black max-height-300">
                         <h4>Tino</h4>
                     </div>
-                    <div class="col-md-5 text-center padding-10 margin-20 shadow-black">
+                    <div class="col-md-5 text-center padding-10 margin-20 shadow-black max-height-300">
                         <h4>outfit of the day</h4>
                     </div>
                 </div>
